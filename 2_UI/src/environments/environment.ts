1e53c6fb462b18386fd305309e919e12082648e1
EC2 Default User <ec2-user@ip-172-31-19-95.us-west-1.compute.internal>
2022-04-03 03:24:18 +0000

    First commit

--- 2_UI/src/environments/environment.ts
+++ 2_UI/src/environments/environment.ts
@@ -3,20 +3,20 @@ export const environment = {
   production: true,
 
   // TODO: make sure you have the correct region
-  region: 'eu-west-1',
+  region: 'us-west-1',
 
 
   // TODO: This id can be retrieved in output section of the cognito ui
   // cloud formation stack.
-  cognitoIdentityPoolId: '',
+  cognitoIdentityPoolId: 'us-west-1:88d13cc6-28ac-479d-94fd-84ab0c21d36e',
 
   // TODO: Facebook app id can be retrieved from the application in your
   // facebook developer account.
-  facebookAppId: '',
+  facebookAppId: '315677187129123',
 
   // TODO: The API URL is available in the API Gateway console under Stage
   // NOTE: don't forget trailing "/"  For example:
   // https://api.example.com/prod/
-  ticketAPI: ''
+  ticketAPI: 'https://api.nghinguyen01sjsu.email/'
 
 };
